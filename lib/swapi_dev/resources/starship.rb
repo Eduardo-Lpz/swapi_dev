e68f8da27ab02f32bc3386a78ea390dcd903955c
Eduardo Lopez <jramos@arkusnexus.com>
2021-12-29 10:55:57 -0800

    resources added

---
+++ lib/swapi_dev/resources/starship.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module SwapiDev
+  class Starship < Endpoint
+  end
+end
---
+++ lib/swapi_dev/resources/vehicle.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module SwapiDev
+  class Vehicle < Endpoint
+  end
+end
